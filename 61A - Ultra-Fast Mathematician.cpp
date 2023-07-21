9226071a0c6c616f72fa86267190f9fed95b377e
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-07-21 15:06:19 +0600

    Update 61A - Ultra-Fast Mathematician.cpp

--- 61A - Ultra-Fast Mathematician.cpp
+++ 61A - Ultra-Fast Mathematician.cpp
@@ -26,4 +26,34 @@ int main ()
 
     return 0;
 }
+//same way 
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string s1,s2;
+    cin>>s1>>s2;
+    for(int i=0,j=0;i<s1.size(),j<s2.size();i++,j++)
+    {
+        if(s1[i]!=s2[j])
+        {
+            cout<<'1';
+        }
+        else if(s1[i]==s2[j])
+        {
+            cout<<'0';
+        }
+
+
+    }
+   // cout<<s2<<endl;
+
+
+    return 0;
+}
+
 
