bde4fc7945a20bbe401734590b3668062a539c17
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-07-28 20:42:40 +0600

    Update hulk.cpp

--- hulk.cpp
+++ hulk.cpp
@@ -31,4 +31,33 @@ int main ()
 
     return 0;
 }
-
+//new way
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==1 && i==1)
+        {
+            cout<<"I hate ";
+        }
+        else if(i%2==0)
+        {
+            cout<<"that I love ";
+        }
+        else if(i%2==1)
+        {
+            cout<<"that I hate ";
+        }
+    }
+    cout<<" "<<"it";
+ 
+ 
+    return 0;
+}
