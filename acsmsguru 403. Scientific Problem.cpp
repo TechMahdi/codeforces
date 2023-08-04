38bfbaa7daf91e6630cbebec7f47f25340bbb3fc
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-04 22:13:18 +0600

    Create acsmsguru 403. Scientific Problem.cpp

---
+++ acsmsguru 403. Scientific Problem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long long n;
+    cin>>n;
+  cout<<(2*n+1);
+ 
+ 
+    return 0;
+}
