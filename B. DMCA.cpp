d1af51f74c21e1f3b7687cfced73ecc944030ae7
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-12 22:15:51 +0600

    Create B. DMCA.cpp

---
+++ B. DMCA.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define fn for(i=0;i<n;i++)
+#define fj for(j=0;j<n;j++)
+#define PI 3.141592653589793
+#define maxa *max_element
+#define mina *min_element
+#define fast  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define cina(a,n) for(ll i=0;i<n;i++){cin>>a[i];}
+#define cin1 for(ll i=0;i<n;i++){cin>>a[i];}
+#define couta(a,n) for(ll i=0;i<n;i++){cout<<a[i]<<" ";}
+#define vec vector<ll> v
+#define all v.begin(),v.end()
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+int main ()
+{
+    fast
+   long long n;
+   cin>>n;
+   cout<< ((n-1)%9)+1;
+
+    return 0;
+}
